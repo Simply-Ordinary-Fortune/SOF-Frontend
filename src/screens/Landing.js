817d77103f218fd0252fdc0317b04efdc9d116ca
seulgibaek <seulgi.baek@dgu.ac.kr>
2025-02-15 09:43:43 +0900

    [feat] guestId 없을 시 redirect하는 랜딩페이지

--- src/screens/Landing.js
+++ src/screens/Landing.js
@@ -1,27 +1,45 @@
 import React from 'react';
-import {Text, TouchableOpacity, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import SignButton from './SignButton'; // SignButton 컴포넌트를 임포트
 
-export default function SignButton({buttonName, onPress}) {
+const LandingPage = ({navigation}) => {
   return (
-    <TouchableOpacity style={styles.buttonContainer} onPress={onPress}>
-      <Text style={styles.buttonText}>{buttonName}</Text>
-    </TouchableOpacity>
+    <View style={styles.container}>
+      <Text style={styles.headerText}>환영합니다!</Text>
+      <Text style={styles.descriptionText}>
+        계속하려면 로그인 또는 게스트 계정을 생성 해주세요.
+      </Text>
+      <SignButton
+        buttonName="로그인"
+        onPress={() => navigation.navigate('LoginScreen')} // 로그인 페이지로 이동
+      />
+      <SignButton
+        buttonName="회원가입"
+        onPress={() => navigation.navigate('SignupScreen')} // 회원가입 페이지로 이동
+      />
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  buttonContainer: {
-    borderRadius: 10,
-    backgroundColor: '#2ECC71',
-    paddingVertical: 12,
-    paddingHorizontal: 20,
-    marginVertical: 10,
-    width: 200,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#f4f4f4',
   },
-  buttonText: {
-    color: 'white',
-    fontSize: 16,
+  headerText: {
+    fontSize: 24,
     fontWeight: 'bold',
+    marginBottom: 12,
+  },
+  descriptionText: {
+    fontSize: 16,
+    color: '#666',
+    marginBottom: 20,
+    textAlign: 'center',
   },
 });
+
+export default LandingPage;
